95a54d1406a64fc01214a4a12fc7b6e24ab4d754
joexyi <joeyi@unc.edu>
2018-08-27 16:54:23 -0400

    Lab 1

---
+++ Lab 1.R
@@ -0,0 +1,21 @@
+# Chapter 3 for R in Data Science
+# Joe Yi
+
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y=hwy))
+
+ggplot(data = mpg)
+?mpg
+
+install.packages("rmarkdown")
+library(rmarkdown)
+
+install.packages("stringi")
+library(stringi)
+
+library(knitr)
+
+
+?mpg
+
+library(tidyverse)
+mpg
